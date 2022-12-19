9b10477c7174f3a85d767cb242395a73de5eea28
BillC424 <wcurran44@gmail.com>
2022-12-19 15:18:02 -0500

    Modified Node class to include Comparable module. This involved adding the data instance variable to be accessible using attr_reader and modifying the class method for <=>

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,7 +1,7 @@
 class Node
-
   include Comparable
-  # need to read up on Comparable module to determine how to compare nodes using data attribute
+
+  attr_reader :data
 
   def initialize(data)
     @data = data
@@ -9,30 +9,38 @@ class Node
     @right_child = nil
   end
 
-  def ==(other_node)
-    node.data == other_node.data
+  def <=>(other_node)
+    data <=> other_node.data
   end
-
 end
+# class Tree
+#
+#   #maybe this attribute needs to be a method instead?
+#   attr_accessor :root
+#
+#   def initialize(array)
+#     @array = array
+#   end
+#
+#   def build_tree(array)
+#     #base case
+#     if array.kind_of?(array) == false
+#       return
+#     else
+#       #base case for comparing node values
+#
+#       #find middle of array
+#       middle = array / 2
+#     #recursively calling method on left
+#     #recursively calling method on left
+#
+#     # return level 0 root node
+#   end
+#
+# end
+five = Node.new(5)
+three = Node.new(3)
 
-class Tree
+p five.data > three.data
 
-  #maybe this attribute needs to be a method instead? 
-  attr_accessor :root
-
-  def initialize(array)
-    @array = array
-  end 
-  
-  def build_tree(array)
-    #base case
-
-    #recursively calling method on left
-    #recursively calling method on left
-
-    # return level-0 root node 
-  end
-
-end
-
-array = [1,2,3,4,5,6,7,8,9]
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
